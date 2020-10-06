2067921226c16d6416f2c87e64780428c414d3c2
CloudWannabe <55220508+CloudWannabe@users.noreply.github.com>
2020-10-05 23:13:01 -0700

    Update AddingMachine.java
    Part 2

--- AddingMachine.java
+++ AddingMachine.java
@@ -2,9 +2,11 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  StringBuffer history;
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    history = new StringBuffer("0");
   }
   
   public int getTotal () {
@@ -12,15 +14,31 @@ public class AddingMachine {
   }
   
   public void add (int value) {
+    total = total + value;
+    history.append(" + " + value);
   }
 
   public void subtract (int value) {
+    total = total - value;
+    history.append(" + " + value);
   }
+  
+  public void multiply (int value) {
+    total = total * value;
+    history.append(" * " + value);
+  }
+  
+  public void divide(int value) {
+		total = total / value;
+		history.append(" / " + value);
+	}
 
   public String toString () {
-    return "";
+    return history.toString();
   }
 
   public void clear() {
+    history.delete(0, history.length());
+    history.append("0"); //I added this assuming it would want to restart
   }
 }
